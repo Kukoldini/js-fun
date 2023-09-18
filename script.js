a9feae745bde32ed32fd3dc0244ce357fd7b9dad
Alex <130656294+Kukoldini@users.noreply.github.com>
2023-09-18 19:27:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,101 +3,51 @@ const paragraphNode = document.querySelector('.text');
 const paragraphParent = document.querySelector('.terminal-text');
 
 const nodeCloning = (contentText) => {
+
   let clonedNode = paragraphNode.cloneNode();
   clonedNode.textContent = contentText;
   document.querySelector('.terminal-text').appendChild(clonedNode);
+
 }
 
 receiveBtn.addEventListener('click', () => {
 
-    nodeCloning("Receiving data...");
+  nodeCloning("Receivind data...");
 
-    const dataRetriever = new Promise((resolve, reject) => {
-
+  const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-
       nodeCloning("Prepairing data...");
 
       const serverData = {
-        
-        port: 2080,
-        IP: "181.414.75.82",
-        host: "EthernetWalkers"
-
+        space: "Russia",
+        IP: "187.315.86.17",
+        host: "Eternet Walkers"
       }
-      
-      resolve(serverData);
-
+      resolve(serverData)
     }, 2000)
-
-  });
-
-
-  dataRetriever.then(data => {
-
-    const p = new Promise((resolve, reject) => {
-
-      setTimeout(() => {
-
-        data.modified = true;
-        resolve(data);
-
-      }, 2000);
-
-    }).then(clientData => {
-
-      nodeCloning("Modification data...");
-      setTimeout(() => {
-
-        nodeCloning("Data received: ");
-        for(let prop in data) {
-          nodeCloning(prop + ": " + data[prop]);
-        }
-      
-      }, 2000)
+  }).then(data => {
     
-    });
+    const p = new Promise((resolve, reject) => {
+      
+      setTimeout(() => {
 
+        nodeCloning("Modification data...");
+        data.modify = true;
+        resolve(data)
+      }, 2000)
+
+    }).then(modifiedData => {
+      setTimeout(() => {
+
+        nodeCloning("Data prepared: ");
+        for(let prop in modifiedData) {
+          nodeCloning(prop + ": " + modifiedData[prop]);
+        }
+        nodeCloning("____________________________");
+      }, 2000)
+    })
   })
 
 });
 
-// const dataRetriever = new Promise((resolve, reject) => {
 
-//   setTimeout(() => {
-
-//     console.log("Preparing data...");
-
-//     const serverData = {
-      
-//       port: 2080,
-//       IP: "181.414.75.82",
-//       host: "EthernetWalkers"
-
-//     }
-    
-//     resolve(serverData);
-
-//   }, 2000)
-
-// });
-
-
-// dataRetriever.then(data => {
-
-//   const p = new Promise((resolve, reject) => {
-
-//     setTimeout(() => {
-
-//       data.modified = true;
-//       resolve(data);
-
-//     }, 2000);
-
-//   }).then(clientData => {
-
-//     console.log("Data received", clientData);
-  
-//   });
-
-// })
